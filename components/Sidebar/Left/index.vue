696110dc59472d0e736282e2d510afb0a026de62
jinyong.park <jiny3360@nine2021.com>
2022-10-18 15:50:55 +0900

    feature/02._frontend_sidebar - Left sidebar 추가

---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20" :class="defaultTransition">
+      <nuxt-link to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+      <div class="mt-2 space-y-3">
+
+        <SidebarLeftTab active>
+          <template v-slot:icon>
+            <HomeIcon />
+          </template>
+          <template v-slot:name>
+            Home
+          </template>
+        </SidebarLeftTab>
+
+        <SidebarLeftTab>
+          <template v-slot:icon>
+            <HashtagIcon />
+          </template>
+          <template v-slot:name>
+            Explore
+          </template>
+        </SidebarLeftTab>
+
+
+        <SidebarLeftTab>
+          <template v-slot:icon>
+            <BellIcon />
+          </template>
+          <template v-slot:name>
+            Notifications
+          </template>
+        </SidebarLeftTab>
+
+
+        <SidebarLeftTab>
+          <template v-slot:icon>
+            <InboxIcon />
+          </template>
+          <template v-slot:name>
+            Messages
+          </template>
+        </SidebarLeftTab>
+
+        <SidebarLeftTab>
+          <template v-slot:icon>
+            <BookmarkIcon />
+          </template>
+          <template v-slot:name>
+            Bookmarks
+          </template>
+        </SidebarLeftTab>
+
+
+        <SidebarLeftTab>
+          <template v-slot:icon>
+            <DocumentTextIcon />
+          </template>
+          <template v-slot:name>
+            Lists
+          </template>
+        </SidebarLeftTab>
+
+
+        <SidebarLeftTab>
+          <template v-slot:icon>
+            <UserIcon />
+          </template>
+          <template v-slot:name>
+            Profile
+          </template>
+        </SidebarLeftTab>
+
+        <SidebarLeftTab>
+          <template v-slot:icon>
+            <PlusIcon />
+          </template>
+          <template v-slot:name>
+            More
+          </template>
+        </SidebarLeftTab>
+      </div>
+        <div class="hidden xl:block">
+          <UiButton liquid size="lg" @on-click="emits('onTweet')">
+                    <span class="font-bold">
+                        Tweet
+                    </span>
+          </UiButton>
+        </div>
+        <div class="block xl:hidden">
+          <UiButton @on-click="emits('onTweet')">
+            <div class="w-6 h-6 font-bold">
+              <PencilIcon />
+            </div>
+          </UiButton>
+        </div>
+
+    </div>
+</template>
+
+
+<script setup>
+import UseTailwindConfig from '../../../composables/useTailwindConfig'
+import {HashtagIcon,HomeIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  DocumentTextIcon,
+  UserIcon,
+  PlusIcon,
+  PencilIcon } from '@heroicons/vue/24/solid'
+
+const { defaultTransition } = UseTailwindConfig()
+
+</script>
